d5b25c41e27e1c175c516e37b6c82999ed8fd28a
Waizung Taam <waizungtaam@gmail.com>
2019-07-25 14:59:36 +0800

    Added flask_wtf

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
+login = LoginManager()
 
 def create_app(config=Config):
     app = Flask(__name__)
@@ -12,6 +14,7 @@ def create_app(config=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login.init_app(app)
 
     @app.route('/')
     def index():
