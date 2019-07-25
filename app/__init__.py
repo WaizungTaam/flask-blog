c113b4d6e89d3d3103ceee2bc24ea2ba999b4857
Waizung Taam <waizungtaam@gmail.com>
2019-07-25 14:35:21 +0800

    First commit

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from config import Config
+
+def create_app(config=Config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    @app.route('/')
+    def index():
+        return 'Hello World'
+
+    return app
