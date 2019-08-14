cb51a6900c86e8cbccdf2627c104ec8d1a432f16
Waizung Taam <waizungtaam@gmail.com>
2019-08-14 17:39:39 +0800

    Added profiler

---
+++ profile_.py
@@ -0,0 +1,7 @@
+from werkzeug.contrib.profiler import ProfilerMiddleware
+from app import create_app
+
+app = create_app()
+app.config['PROFILE'] = True
+app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
+app.run(debug=True)
